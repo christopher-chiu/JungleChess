df975226bdd3cedaedd8ba0fe6c1418a5daa727e
Christopher Chiu <50348744+christopher-chiu@users.noreply.github.com>
2021-04-24 19:07:08 -0400

    Add the pieces

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -12,8 +13,10 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Dou Shou Qi (Jungle Chess)");
+        primaryStage.getIcons().add(new Image("resources/img/icon.png"));
+
         primaryStage.setScene(new Scene(root));
-//        primaryStage.setResizable(false);
+        primaryStage.setResizable(false);
         primaryStage.show();
     }
 
